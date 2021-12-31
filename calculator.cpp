eb31db2ec779f8e4483e153d9f34444521a2edf3
shamanth <hshamanth60@gmail.com>
2021-12-31 20:40:16 +0530

    Refactored Calculator code by seperating the logic to perform Addition

--- calculator.cpp
+++ calculator.cpp
@@ -7,20 +7,29 @@ int add(int a, int b)
 }
 int subtraction(int a, int b);
 int multiply(int a, int b);
+double divide(int a, int b);
 int main()
 {
 	int a=0, b=0;
 	cout<<"enter the two numbers"<<endl;
 	cin>>a>>b;
     	cout<<"Addition: "<<add(a,b)<<endl;
-    	cout<<"Subtraction: "<<add(a,b)<<endl;
+    	cout<<"Subtraction: "<<subtraction(a,b)<<endl;
+	cout<<"Multiply: "<<multiply(a,b)<<endl;
+	cout<<"Divide: "<<divide(a,b)<<endl;
 }
-//function to subtract two numbers
+//subtract two numbers
 int subtraction(int a, int b)
 {
 	return a-b;
 }
+//multiply two numbers
 int multiply(int a,int b)
 {
 	return a*b;
+}
+//divide two numbers
+double divide(int a, int b)
+{
+	return a/b;
 }
